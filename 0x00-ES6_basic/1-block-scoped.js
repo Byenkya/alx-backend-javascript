1f4ceb0f44aa0601c0501ab5b4e52b00b4fa6dba
Byenkya <jbyenkya@idi.co.ug>
2022-12-19 17:09:01 +0300

    changesetjr

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,12 @@
-#!/usr/bin/env node
-
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
   
     if (trueOrFalse) {
-      // eslint-disable-next-line
+      /* eslint-disable no-unused-vars */
       const task = true;
-      // eslint-disable-next-line
       const task2 = false;
+      /* eslint-enable no-unused-vars */
     }
   
     return [task, task2];
